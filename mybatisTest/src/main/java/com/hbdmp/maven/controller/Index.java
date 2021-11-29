34b9d3957d7bfb899e70414fcdb984e5eee26a10
yjb <1024763525@qq.com>
2021-11-29 15:52:33 +0800

    优化配置

--- mybatisTest/src/main/java/com/hbdmp/maven/controller/Index.java
+++ mybatisTest/src/main/java/com/hbdmp/maven/controller/Index.java
@@ -1,34 +1,24 @@
 package com.hbdmp.maven.controller;
 
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.Mapping;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
-
-import javax.security.auth.login.Configuration;
 
 /**
  * author yjb
  * create 2021/9/30 - 16:00
  */
 
-@RestController("/index")
+@RestController
+@RequestMapping("/index")
 public class Index {
 
-    @PostMapping("/test")
-    public void testMyConfig(){
-        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext();
-    }
-
-    @GetMapping("/hello")
-    public void testhello(){
-        System.out.println("-----hello-----");
-    }
-    @GetMapping("/hello2")
-    public void testhello2(){
-        System.out.println("-----hello2-----");
+    @GetMapping(value = "hello",produces = org.springframework.http.MediaType.APPLICATION_JSON_VALUE)
+    @ResponseBody
+    public String testhello(){
+        String s = "-----hello-----";
+        System.out.println(s);
+        return s;
     }
 }
